e40d40a221ba76df4874a311d84f11dd70193a41
overano <overano@atsistemas.com>
2018-12-07 13:23:46 +0100

    initial commit
    - proyecto microservicio usando en gradle.
    - servicio rest, que tienen un metodo dumb que devuelve el contenido que se le envia.
    - dockerizacion del jar, imagen creada "com.at/netlifx"
    
    comandos para crear la imagen de docker:
    - dockerClean.
    - docker.

---
+++ src/main/java/com/at/netflix/NetflixApplication.java
@@ -0,0 +1,12 @@
+package com.at.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.at.netflix.NetflixApplication.class, args);
+	}
+}
---
+++ src/main/java/com/at/netflix/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.at.netflix.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/user")
+public class UserController {
+
+    @PostMapping("/add")
+    public String create(@RequestBody String user){
+        return user;
+    }
+}
